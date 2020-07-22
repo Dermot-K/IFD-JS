5551ab2a96ffdc5c7d00a68e4615bfe1c1a53a0b
Dermot-K <dermokeelan@gmail.com>
2020-07-22 13:49:36 +0000

    table rows added, data truncated

--- main.js
+++ main.js
@@ -36,9 +36,11 @@ function writeToDocument(type) {
             var dataRow = [];
 
             Object.keys(item).forEach(function(key) {
-                dataRow.push(`<td>${item[key]}</td>`);
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 25);
+                dataRow.push(`<td>${truncatedData}</td>`);
             });
-            tableRows.push(dataRow);
+            tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
         el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
