08deaeb730536e8402d0f6722a4d1cd6ab48358e
Dermot-K <dermokeelan@gmail.com>
2020-07-22 11:04:53 +0000

    data in tabular format

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
 const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
 function getData(type, cb) {
-  var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", baseURL + type + "/");
-  xhr.send();
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      cb(JSON.parse(this.responseText)); //runs the function that we pass in as a callback - when getData called function will be executed here with this as the data argument
-    }
-  };
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText)); //runs the function that we pass in as a callback - when getData called function will be executed here with this as the data argument
+        }
+    };
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function (key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`
 }
 
 function writeToDocument(type) {
-    getData(type, function(data) {
-        document.getElementById("data").innerHTML = data;
-    })
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function (data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function (item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+    });
 }
 
